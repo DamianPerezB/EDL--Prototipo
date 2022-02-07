80a5d85db48f2e1082eaee40c2d96841b2c512b8
DamianPerezB <89427173+DamianPerezB@users.noreply.github.com>
2022-02-06 18:00:33 -0600

    Update refrigerador.h

--- refrigerador.h
+++ refrigerador.h
@@ -13,6 +13,7 @@ typedef struct Producto{
     int mesCaducidad;
     int diaCaducicad;
     int disponibilidad;
+    int identificador;
 }Producto;
 
 typedef struct Nodo{
@@ -25,7 +26,9 @@ typedef struct ListaSimple{
 }ListaSimple;
 
 //Funciones para generar las listas
-Producto *agregar_Producto (int TIPO);
+Producto *agregar_Producto (int TIPO, int ID);
+Producto *agregar_Producto_temporal(float elemento);
+void copiar_Producto(Nodo *aux, Nodo *aux2);
 Nodo *crear_Nodo (Producto *producto);
 ListaSimple *crear_ListaSimple ();
 
@@ -35,10 +38,15 @@ void insertar_cabeza (ListaSimple *lista, Nodo *aux);
 void insertar_cola (ListaSimple *lista, Nodo *aux);
 void insertar_en_medio (ListaSimple *lista, Nodo *anterior, Nodo *elemento);
 void quitar_cabeza (ListaSimple *lista);
-void quitar_cola (ListaSimple *lista);
+void quitar_cola (ListaSimple *lista, Nodo *cola);
 void quitar_en_medio (ListaSimple *lista, Nodo *elemento);
-Nodo *buscar_Elemento (ListaSimple *lista, Nodo *elemento);
+Nodo *buscar_Elemento_Recursivo (ListaSimple *lista, int superior, Nodo *elemento);
+ListaSimple *buscar_Elemento_Recursivo_2 (ListaSimple *lista, ListaSimple *listaTemporal, int inferior, int superior, Nodo *elemento);
 void vaciar_Lista (ListaSimple *lista);
 void imprimir_Lista (ListaSimple *lista);
 
 //Funciones de ordenamiento
+ListaSimple *QuickSort (ListaSimple *lista, int inicio, int final);
+ListaSimple *Mergesort (ListaSimple *lista, int inicio, int final);
+void mezcla (ListaSimple *lista, int izquierda, int central, int derecha);
+void *ordenamiento_seleccion(ListaSimple *lista, int n);
